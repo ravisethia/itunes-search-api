427e6e1a1583172351bdfe5ec6e09fd5f0395aa9
Sethia, Ravi <sethia.ravi2@gmail.com>
2019-07-19 15:29:27 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
-var path = require('path');
-var serveStatic = require('serve-static');
 var app = express();
-app.use(serveStatic(__dirname + "/"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
     console.log("app running on port.", server.address().port);
 });
